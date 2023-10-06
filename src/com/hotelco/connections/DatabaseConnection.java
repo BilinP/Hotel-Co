4bf004959f1fc99e727cb787259bee5294d6ea12
DanielSchwartzCS <98420686+DanielSchwartzCS@users.noreply.github.com>
2023-10-05 17:20:37 -0700

    Create DatabaseConnection.java

---
+++ src/com/hotelco/connections/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package com.hotelco.connections;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    
+    Connection con = null;
+
+    public static Connection connectDB()
+{
+    try
+    {
+        Class.forName("com.mysql.jdbc.Driver");
+ 
+        Connection con = DriverManager.getConnection(
+            "jdbc:mysql://sql3.freemysqlhosting.net:3306",
+            "sql3651321", "fB8SKw8fPQ");
+              
+        return con;
+    }
+
+    catch (SQLException | ClassNotFoundException e)
+    {
+        System.out.println(e);
+
+        return null;
+    }
+}
+}
