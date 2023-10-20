1e3d1d06eccfcdb253baa50b02213f069ef44036
GAzaCSUN <grigor.azakian.22@my.csun.edu>
2023-10-20 16:22:47 -0700

    Added JavaDocs.

--- src/com/hotelco/controllers/SearchController.java
+++ src/com/hotelco/controllers/SearchController.java
@@ -14,32 +14,71 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
+/**
+ * The SearchController class is the associated controller class of the 'SearchGUI' view. 
+ * It handles connection between the GUI and internal data.
+ * 
+ * @author      Grigor Azakian
+ * @version     %I%, %G%
+ */
 public class SearchController extends BaseController {
 
+    /**
+     * DatePicker that contains the date the user wants to be the first day of their booking.
+     */
     @FXML
     private DatePicker startDate;
 
+    /**
+     * DatePicker that contains the date the user wants to be the last day of their booking.
+     */
     @FXML
     private DatePicker endDate;
 
+    /**
+     * Text that contains the number of guests.
+     */
     @FXML
     private Text guests;
 
+    /**
+     * Text that can be displayed to notify the user of any potential invalid inputs.
+     */    
     @FXML
     private Text notification;
 
+    /**
+     * Button that allows a user to book a king room.
+     * Calls 'createBooking()' upon being pressed.
+     */
     @FXML
     private Button king;
 
+    /**
+     * Button that allows a user to book a queen room.
+     * Calls 'createBooking()' upon being pressed.
+     */
     @FXML
     private Button queen;
 
+    /**
+     * Button that allows a user to book a suite.
+     * Calls 'createBooking()' upon being pressed.
+     */    
     @FXML
     private Button suite;
 
+    /**
+     * Button that allows a user to book a double.
+     * Calls 'createBooking()' upon being pressed.
+     */    
     @FXML
     private Button dbl;
 
+    /**
+     * This method is called immediately upon controller creation.
+     * It assigns data to several buttons so that it will be easier to tell which button was pressed.
+     */
     @FXML
     private void initialize() {
         Platform.runLater(() -> {
@@ -50,6 +89,14 @@ public class SearchController extends BaseController {
         });
     }
 
+    /**
+     * This method is called when pressing the 'Check Availability' button.
+     * It handles input verification and checks for available rooms with the given parameters.
+     * It will enable buttons corresponding to which rooms are available.
+     * @param event The 'mouse released' event that is triggered by pressing the 'Check Availability' button.
+     * @author Grigor Azakian
+     * @author Daniel Schwartz
+     */
     @FXML
     private void checkAvailability(MouseEvent event) {
         Integer numGuests = Integer.parseInt(guests.getText());
@@ -86,6 +133,13 @@ public class SearchController extends BaseController {
         }
     }
 
+    /**
+     * This method is called when pressing the 'Book' button.
+     * It will create a booking in the class ReservationSystem for the corresponding user.
+     * @param event The 'mouse released' event that is triggered by pressing the 'Book' button.
+     * @author Grigor Azakian
+     * @author Daniel Schwartz
+     */
     @FXML
     private void createBooking(MouseEvent event) {
         Button pressedButton = (Button) event.getSource();
@@ -103,6 +157,11 @@ public class SearchController extends BaseController {
         
     }
 
+    /**
+     * This method is called by pressing the '-' text to the left of the guest number.
+     * It decrements the current guest number as long as the current guest number is greater than '1'.
+     * @param event The 'mouse released' event that is triggered by pressing the '-' text to the left of the guest number.
+     */
     @FXML
     private void decrementGuest(MouseEvent event) {
         if (Integer.parseInt(guests.getText()) > 1) {
@@ -110,6 +169,13 @@ public class SearchController extends BaseController {
         }
     }
 
+    /**
+     * This method is called by pressing the '+' text to the right of the guest number.
+     * It increments the current guest number as long as it is below the maximum amount of allowed guests.
+     * @param event The 'mouse released' event that is triggered by pressing the '+' text to the right of the guest number.
+     * @author Grigor Azakian
+     * @author Daniel Schwartz
+     */
     @FXML
     private void incrementGuest(MouseEvent event) {
         if (Integer.parseInt(guests.getText()) < Constants.MAX_CAP) {
@@ -117,6 +183,11 @@ public class SearchController extends BaseController {
         }
     }
 
+    /**
+     * This method is called by pressing the 'Go Back' text.
+     * It exits the 'SearchGUI' and enters the 'MenuGUI'.
+     * @param event The 'mouse released' event that is triggered by pressing the 'Go Back' text.
+     */
     @FXML
     private void switchToMenuScene(MouseEvent event) {
         switchScene(FXMLPaths.MENU, event);
