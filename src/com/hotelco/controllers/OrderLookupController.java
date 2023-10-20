3caa7ddb9dfb298cec94f4de9eb87950ccdf26e5
GAzaCSUN <grigor.azakian.22@my.csun.edu>
2023-10-20 16:03:36 -0700

    Added JavaDocs.

--- src/com/hotelco/controllers/OrderLookupController.java
+++ src/com/hotelco/controllers/OrderLookupController.java
@@ -1,7 +1,5 @@
 package com.hotelco.controllers;
 
-import java.time.format.DateTimeFormatter;
-
 import com.hotelco.entities.Reservation;
 import com.hotelco.entities.ReservationSystem;
 import com.hotelco.utilities.FXMLPaths;
@@ -11,26 +9,56 @@ import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
+/**
+ * The OrderLookupController class is the associated controller class of the 'OrderLookupGUI' view. 
+ * It handles connection between the GUI and internal data.
+ * 
+ * @author      Grigor Azakian
+ * @version     %I%, %G%
+ */
+
 public class OrderLookupController extends BaseController {
 
+    /**
+     * Text that will display a reservation at the top of the list.
+     */
     @FXML
     private Text firstOrder;
 
+    /**
+     * Text that will display a reservation below 'firstOrder'.
+     */
     @FXML
     private Text secondOrder;
 
+    /**
+     * Text that will display a reservation below 'secondOrder'.
+     */
     @FXML
     private Text thirdOrder;
 
+    /**
+     * Text that will display a reservation below 'thirdOrder'.
+     */
     @FXML
     private Text fourthOrder;
 
+    /**
+     * Text that will display a reservation below 'fourthOrder'.
+     */
     @FXML
     private Text fifthOrder;
 
+    /**
+     * Text that will display the current page number.
+     */
     @FXML
     private Text pageNumber;
 
+    /**
+     * This method is called immediately upon controller creation.
+     * It calls 'displayOrders()' to immediately display a users reservations.
+     */
     @FXML
     private void initialize() {
         Platform.runLater(() -> {
@@ -38,16 +66,26 @@ public class OrderLookupController extends BaseController {
         });
     }
 
+    /**
+     * This method is called by pressing the '-' text to the left of the page number.
+     * It decrements the current page number as long as the current page number is greater than '1'.
+     * @param event The 'mouse released' event that is triggered by pressing the '-' text to the left of the page number.
+     */
     @FXML
-    void decrementPage(MouseEvent event) {
+    private void decrementPage(MouseEvent event) {
         if (Integer.parseInt(pageNumber.getText()) > 1) {
             pageNumber.setText(Integer.toString(Integer.parseInt(pageNumber.getText()) - 1));
         }
         displayOrders();
     }
 
+    /**
+     * This method is called by pressing the '+' text to the right of the page number.
+     * It increments the current page number as long as there are available reservations to show.
+     * @param event The 'mouse released' event that is triggered by pressing the '+' text to the right of the page number.
+     */
     @FXML
-    void incrementPage(MouseEvent event) {
+    private void incrementPage(MouseEvent event) {
         Reservation[] reservations = ReservationSystem.getCurrentUser().getReservations();
         if (Integer.parseInt(pageNumber.getText()) < Math.ceil(reservations.length/5.0)) {
             pageNumber.setText(Integer.toString(Integer.parseInt(pageNumber.getText()) + 1));
@@ -55,11 +93,19 @@ public class OrderLookupController extends BaseController {
         displayOrders();
     }
 
+    /**
+     * This method is called by pressing the 'Go Back' text.
+     * It exits the 'SearchGUI' and enters the 'MenuGUI'.
+     * @param event The 'mouse released' event that is triggered by pressing the 'Go Back' text.
+     */
     @FXML
     private void switchToMenuScene(MouseEvent event) {
         switchScene(FXMLPaths.MENU, event);
     }
 
+    /**
+     * This method displays a logged in users reservation history corresponding to the page number.
+     */
     private void displayOrders() {
         Integer index, j;
         Reservation[] reservations = ReservationSystem.getCurrentUser().getReservations();
