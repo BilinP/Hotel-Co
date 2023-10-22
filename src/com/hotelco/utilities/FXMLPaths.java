a3a3f44441edf8d6d9eb544a0c8da1c98dbd54e4
GAzaCSUN <grigor.azakian.22@my.csun.edu>
2023-10-22 09:02:42 -0700

    Updated FXMLPaths to reflect name changes and new additions

--- src/com/hotelco/utilities/FXMLPaths.java
+++ src/com/hotelco/utilities/FXMLPaths.java
@@ -1,9 +1,17 @@
+/**
+ * The FXMLPaths class contains the file paths for all FXML files.
+ * 
+ * @author      Grigor Azakian
+ * @version     %I%, %G%
+ */
 package com.hotelco.utilities;
 
 public class FXMLPaths {
     public static final String LOGIN = "/com/hotelco/views/LoginGUI.fxml";
     public static final String CREATE_ACCOUNT = "/com/hotelco/views/CreateAccountGUI.fxml";
     public static final String MENU = "/com/hotelco/views/MenuGUI.fxml";
-    public static final String ORDER_LOOKUP = "/com/hotelco/views/OrderLookupGUI.fxml";
-    public static final String SEARCH = "/com/hotelco/views/SearchGUI.fxml";
+    public static final String ORDER_LOOKUP = "/com/hotelco/views/ReservationHistoryGUI.fxml";
+    public static final String SEARCH = "/com/hotelco/views/RoomSearchGUI.fxml";
+    public static final String THANK_YOU = "/com/hotelco/views/ThankYouGUI.fxml";
+    public static final String RESERVATION_LOOKUP = "/com/hotelco/views/ReservationLookupGUI.fxml";
 }
