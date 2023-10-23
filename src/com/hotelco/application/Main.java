d0fa9b0e6b447ca104103282463c82aac851f2a3
Bilin Pattasseril <bilinpattasseril@gmail.com>
2023-10-23 15:40:45 -0700

    Fix format and added icon

--- src/com/hotelco/application/Main.java
+++ src/com/hotelco/application/Main.java
@@ -8,17 +8,19 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-
+		
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource(FXMLPaths.LOGIN));
 			Scene scene = new Scene(root, Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight());
 			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.setTitle("HotelCo");
+			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/com/hotelco/images/hotelco.png")));
 			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
