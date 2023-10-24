5cd45d831441ea63ccbab3026b9aaed959437ecc
Bilin <bilinpattasseril@gmail.com>
2023-10-24 12:10:43 -0700

    Login/Register revamp ui

--- src/com/hotelco/application/Main.java
+++ src/com/hotelco/application/Main.java
@@ -3,7 +3,6 @@ package com.hotelco.application;
 import com.hotelco.utilities.FXMLPaths;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
@@ -12,17 +11,34 @@ import javafx.scene.image.Image;
 
 
 public class Main extends Application {
+	private double xOffset = 0;
+    private double yOffset = 0;
 	@Override
 	public void start(Stage primaryStage) {
 		
+
+		
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource(FXMLPaths.LOGIN));
-			Scene scene = new Scene(root, Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight());
+			Scene scene = new Scene(root, 700, 500);
 			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.setTitle("HotelCo");
 			primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/com/hotelco/images/hotelco.png")));
 			primaryStage.setResizable(false);
+			root.setOnMousePressed(event -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
+
+            root.setOnMouseDragged(event -> {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            });
+
+
+
 			primaryStage.setScene(scene);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/com/hotelco/controllers/BaseController.java
+++ src/com/hotelco/controllers/BaseController.java
@@ -2,10 +2,10 @@ package com.hotelco.controllers;
 
 import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.Node;
 
 /**
@@ -13,9 +13,12 @@ import javafx.scene.Node;
  * It contains common functionality all controllers must use.
  * 
  * @author      Grigor Azakian
+ * @author      Bilin Pattasseril
  * @version     %I%, %G%
  */
-public class BaseController {
+public class BaseController  {
+    private double xOffset = 0;
+    private double yOffset = 0;
     /**
      * This method will switch the scene the user is currently viewing to the provided FXML file located in 'fxmlLocation'.
      * @param fxmlLocation The file path of the FXML file to switch to.
@@ -23,11 +26,28 @@ public class BaseController {
      * @return Returns the controller associated with 'fxmlLocation'. If the method fails to switch the scene, returns null.
      */
     protected BaseController switchScene(String fxmlLocation, Event event) {
+
+        
+
         try {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlLocation));
         Parent root = loader.load();
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root, Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight());
+        double preferredWidth = ((BorderPane) root).getPrefWidth();
+        double preferredHeight = ((BorderPane) root).getPrefHeight();
+        Scene scene = new Scene(root, preferredWidth, preferredHeight);
+        stage.setResizable(false);
+
+        root.setOnMousePressed(mEvent -> {
+            xOffset = mEvent.getSceneX();
+            yOffset = mEvent.getSceneY();
+        });
+
+        root.setOnMouseDragged(mEvent -> {
+            stage.setX(mEvent.getScreenX() - xOffset);
+            stage.setY(mEvent.getScreenY() - yOffset);
+        });
+
         stage.setScene(scene);
         stage.show();
         return loader.getController();
