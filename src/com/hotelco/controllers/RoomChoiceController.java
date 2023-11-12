1a8e14e077f82eb55dd22c3dcd45bdd9f1eff6a0
GAzaCSUN <grigor.azakian.22@my.csun.edu>
2023-11-12 01:07:08 -0800

    New initial booking page and controller

---
+++ src/com/hotelco/controllers/RoomChoiceController.java
@@ -0,0 +1,20 @@
+package com.hotelco.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RoomChoiceController extends BaseController {
+
+    @FXML
+    private Button king;
+
+    @FXML
+    private Button queen;
+
+    @FXML
+    private Button dbl;
+
+    @FXML
+    private Button suite;
+
+}
