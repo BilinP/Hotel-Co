fe54a552f1b734c6a51862211a34ee48bb4d71b0
GAzaCSUN <grigor.azakian.22@my.csun.edu>
2023-11-12 01:06:48 -0800

    Updated to include paths to new GUI

--- src/com/hotelco/utilities/FXMLPaths.java
+++ src/com/hotelco/utilities/FXMLPaths.java
@@ -10,10 +10,11 @@ public class FXMLPaths {
     public static final String LOGIN = "/com/hotelco/views/LoginGUI.fxml";
     public static final String CREATE_ACCOUNT = "/com/hotelco/views/CreateAccountGUI.fxml";
     public static final String MENU = "/com/hotelco/views/MenuGUI.fxml";
-    public static final String ORDER_LOOKUP = "/com/hotelco/views/ReservationHistoryGUI.fxml";
+    public static final String ORDER_LOOKUP = "/com/hotelco/views/OLDReservationHistoryGUI.fxml";
     public static final String SEARCH = "/com/hotelco/views/RoomSearchGUI.fxml";
     public static final String THANK_YOU = "/com/hotelco/views/ThankYouGUI.fxml";
     public static final String RESERVATION_LOOKUP = "/com/hotelco/views/ReservationLookupGUI.fxml";
     public static final String DASHBOARD=  "/com/hotelco/views/dashboard.fxml";
-    public static final String NEWMENU=  "/com/hotelco/views/NEWMenuGUI.fxml";
+    public static final String ROOMS = "/com/hotelco/views/RoomChoiceGUI.fxml";
+    public static final String RHGUI = "/com/hotelco/views/ReservationHistoryGUI.fxml";
 }
