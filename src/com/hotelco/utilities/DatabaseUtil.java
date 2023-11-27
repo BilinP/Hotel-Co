e71046573fc195c450156d5f420f6c462425ada4
JohnKimLee <89819662+JohnKimLee@users.noreply.github.com>
2023-11-27 09:58:04 -0800

    Deleted Manager and moved all methods to DatabaseUtil

--- src/com/hotelco/utilities/DatabaseUtil.java
+++ src/com/hotelco/utilities/DatabaseUtil.java
@@ -11,7 +11,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 /**
  * Utility class to check if user ids or emails exist.
- * @author Daniel Schwartz
+ * @author Daniel Schwartz, John Lee
  */
 public class DatabaseUtil{
 /**
@@ -422,5 +422,140 @@ public class DatabaseUtil{
         ReservationSystem.ready();
         return result;
     }
+    /**
+     * Finds the amount of vacant rooms at the current date
+     * @return the number of vacant rooms
+     */
+    public static Integer findVacantRoomSize(){
+        
+        Integer result = 0;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sqlQuery = "SELECT room_num " + 
+            "FROM rooms " + 
+            "WHERE room_num NOT IN (" + 
+                "SELECT room_num " + 
+                "FROM reservations " + 
+                "WHERE start_date <= '" + Date.valueOf(LocalDate.now()) +
+                "' OR end_date >= '" + Date.valueOf(LocalDate.now()) + "') "; 
+        Connection con = ReservationSystem.getDatabaseConnection();
 
+        try {
+            ps = con.prepareStatement(sqlQuery);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                result++;
+            }
+        }
+        catch (SQLException e){
+            System.out.println("ReservationSystem.findEmptyRoom()");
+            System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+            System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+            System.out.println(e);
+        }
+        ReservationSystem.ready();
+        return result;
+
+    }
+    /**
+     * Gives the amount of revenue made within the current day
+     * @return revenue
+     */
+    public static BigDecimal getTodayRevenue(){
+        
+        BigDecimal result = new BigDecimal(0);
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sqlQuery = "SELECT amount " + 
+            "FROM payments " + 
+            "WHERE time > '" + Date.valueOf(LocalDate.now()) + "'";
+            System.out.println(sqlQuery);
+        Connection con = ReservationSystem.getDatabaseConnection();
+
+        try {
+            ps = con.prepareStatement(sqlQuery);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                result = result.add(rs.getBigDecimal("amount"));
+            }
+        }
+        catch (SQLException e){
+            System.out.println("DatabaseUtil.getTodayRevenue()");
+            System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+            System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+            System.out.println(e);
+        }
+        ReservationSystem.ready();
+        return result;
+    }
+    /**
+     * Gives an integer for the number of check-ins within the current day
+     * @return number of check-ins
+     */
+    public static Integer countTodayCheckIns(){   
+        Integer result = 0;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sqlQuery = "SELECT COUNT(*) AS total " + 
+            "FROM reservations " + 
+            "WHERE startDate = '" + Date.valueOf(LocalDate.now()) + "'";
+            System.out.println(sqlQuery);
+        Connection con = ReservationSystem.getDatabaseConnection();
+
+        try {
+            ps = con.prepareStatement(sqlQuery);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                result = rs.getInt("total");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("DatabaseUtil.countTodayCheckIns()");
+            System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+            System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+            System.out.println(e);
+        }
+        ReservationSystem.ready();
+        return result;
+    }
+    /**
+     * Counts the amount of available rooms within the current day
+     * @return number of available rooms
+     */
+    public static Integer countAvailableRooms(RoomType roomType){
+        Integer result = 0;
+        LocalDate today = LocalDate.now();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sqlQuery =
+            "SELECT COUNT(room_num) as total " +
+            "FROM (" +
+                "SELECT room_num " +
+                "FROM rooms " +
+                "WHERE room_num " +
+                "NOT IN (" +
+                    "SELECT room_num " + 
+                    "FROM reservations " +
+                    "WHERE start_date <= '" + Date.valueOf(today) + "' " +
+                    "AND end_date >= '" + Date.valueOf(today) + "') " +
+                "AND room_type = '" + roomType.toString() + "') " +
+                "AS T";
+        Connection con = ReservationSystem.getDatabaseConnection();
+
+        try {
+            ps = con.prepareStatement(sqlQuery);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                result = rs.getInt("total");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("DatabaseUtil.countAvailableRooms()");
+            System.out.println(Thread.currentThread().getStackTrace()[2].getLineNumber());
+            System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+            System.out.println(e);
+        }
+        ReservationSystem.ready();
+        return result;
+    }
 }
