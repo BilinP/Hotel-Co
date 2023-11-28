a40a13ea7aaddd0903a549fa68dc1e111a2742a3
DanielSchwartzCS <98420686+DanielSchwartzCS@users.noreply.github.com>
2023-11-28 12:18:15 -0800

    Update HardwareControl.java

--- src/com/hotelco/entities/HardwareControl.java
+++ src/com/hotelco/entities/HardwareControl.java
@@ -1,26 +1,9 @@
 package com.hotelco.entities;
 
-import com.hotelco.utilities.RoomCard;
-
 public class HardwareControl {
-
-    private static RoomCard topCard;
     /**
-     * Dummy function to dispense a card.
-     * @param roomCard the card to dispense
+     * Dummy function to magnetize and dispense a card based on a reservation
+     * @param reservation the reservation used to set the card's details
      */
-    public void dispenseNewCard(RoomCard roomCard){
-        topCard.magnetizeCard(roomCard);
-        topCard.dispense();
-    }
-    /**
-     * Dummy function to magnetize a card.
-     * @param roomCard the card to magnetize
-     */
-    public void magnetizeCard(RoomCard roomCard){};
-    /**
-     * Dummy function representing the machine dispensing the top card from
-     * the physical stack of cards
-     */
-    public void dispense(){}
+    public static void dispenseMagnetizedCard(Reservation reservation){}
 }
