2fe913b04074434af4f637f3b3d111553674b055
Bilin <bilinpattasseril@gmail.com>
2023-12-04 02:11:15 -0800

    vancy controller and gui

---
+++ src/com/hotelco/controllers/VacancyController.java
@@ -0,0 +1,71 @@
+package com.hotelco.controllers;
+
+
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
+import javafx.scene.text.Text;
+
+/**
+ * The ProfileController class is the associated controller class of the
+ * 'Profile' view. It handles connection between the GUI and internal data.
+ * 
+ * @author Bilin Pattasseril
+ */
+
+public class VacancyController extends BaseController {
+
+   
+    @FXML
+    private Text doubleInUse;
+
+    @FXML
+    private Text doubleVacant;
+
+    @FXML
+    private Text kingInUse;
+
+    @FXML
+    private Text kingVacant;
+
+    @FXML
+    private Text kingVacant1;
+
+    @FXML
+    private Text queenInUse;
+
+    @FXML
+    private Text queenVacant;
+
+    @FXML
+    private Text suiteInUse;
+
+    @FXML
+    private Text totalCheckIn;
+
+    @FXML
+    private Text totalCheckOut;
+
+    @FXML
+    private Text totalInUse;
+
+    @FXML
+    private Text totalVacant;
+
+    /**
+     * This method is called immediately upon controller creation. It updates the
+     * the current user and then updates the textfields 'email',
+     * 'first','last','number' to what is stored in the database.It as well sets a
+     * formatter for the 'number', 'first', and 'email'.
+     */
+    @FXML
+    private void initialize() {
+      
+
+        Platform.runLater(() -> {
+
+        });
+    }
+
+}
